7d2145c89d3dd4184b30e115845bda060600984c
excavator <str_token@mail.ru>
2019-09-04 21:46:51 +0300

    config Bootstrap sass tuning

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./styles.scss"
 /* jquery,popper явно портировать не обязательно
 надо в head html внести meta из документауии из bootstrap */
-import 'bootstrap';
+import "./bootstrap/bootstrap.js"
 
 jQuery(document).ready(function($){
 
